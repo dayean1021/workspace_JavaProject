f35bd30e57b3cd3884e5c9e6aa166158a85bb6c4
Dayean1021 <s2018s20@e-mirim.hs.kr>
2019-03-28 18:58:07 +0900

    add Echo Project

---
+++ 20190326_Up&Down/src/UpDown.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UpDown {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			int com_num=random.nextInt(100);
+			System.out.print("1~100까지 숫자를 입력하세요 : ");
+			int userNum=sc.nextInt();
+			
+			if ()
+		}
+
+	}
+
+}
