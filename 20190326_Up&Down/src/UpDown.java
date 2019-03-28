da220ec30a3382d7d86d9ec4ea369c84b48797d2
Dayean1021 <s2018s20@e-mirim.hs.kr>
2019-03-28 19:13:08 +0900

    UP&DOWN GAME

--- 20190326_Up&Down/src/UpDown.java
+++ 20190326_Up&Down/src/UpDown.java
@@ -3,15 +3,28 @@ import java.util.Scanner;
 public class UpDown {
 
 	public static void main(String[] args) {
-		public static void main(String[] args) {
+	
 			Scanner sc=new Scanner(System.in);
-			int com_num=random.nextInt(100);
-			System.out.print("1~100까지 숫자를 입력하세요 : ");
-			int userNum=sc.nextInt();
+			int com_num=(int)(Math.random()*100);
+			System.out.println("<<UP & DOWN GAME>>");
 			
-			if ()
-		}
+			while(true) {
+				System.out.print("컴퓨터가 생각한 숫자를 입력하세요 : ");
+				int uesr_Num=sc.nextInt();
+				
+				if (uesr_Num>com_num) {
+					System.out.println("DOWN");
+				}
+				else if(uesr_Num<com_num) {
+					System.out.println("UP");
+				}
+				else if(uesr_Num==com_num) {
+					System.out.println("짝짝 ~정답입니다~ 짝짝");
+					break;
+				}
+			}
+			
+	
 
 	}
-
 }
