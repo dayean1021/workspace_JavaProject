a82a67f2b88f71f108311e0dfbbcb5efb1e5cc83
Dayean1021 <s2018s20@e-mirim.hs.kr>
2019-04-04 19:54:18 +0900

    Nsum

---
+++ 20190404_Nsum/src/Nsum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Nsum {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("각 자리 수의 수를 더한 값을 출력하자");
+		System.out.println("숫자를 입력하세요");
+		String num=sc.nextLine();
+		int length=num.length();
+		int sum=0;
+		
+		
+		for (int i=0;i<length;i++ ) {
+			char n=num.charAt(i);
+			sum+=n-'0'; //0=48
+		}
+		
+		System.out.println("숫자의 합은 : "+sum+"입니다.");
+
+	}
+
+}
